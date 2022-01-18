572469d6b55cfe552dc710ad6e17202afb54e2cb
Woongcheol Shin <1994_100@naver.com>
2022-01-18 15:28:41 +0900

    update silver

---
+++ Silver/2751.py
@@ -0,0 +1,32 @@
+# 1차 시도 : 시간초과
+# N = int(input())
+# number = []
+# for i in range(N):
+#     number.append(int(input()))
+
+# number.sort()
+
+# for num in number:
+#     print(num)
+
+# 2차 시도
+# N = int(input())
+# number = []
+# for i in range(N):
+#     number.append(int(input()))
+
+# list = sorted(number)
+
+# for j in range(len(list)):
+#     print(list[j])
+
+# 3차 시도
+N = int(input())
+number = []
+for i in range(N):
+    number.append(int(input()))
+
+number.sort()
+
+for num in number:
+    print(num)
