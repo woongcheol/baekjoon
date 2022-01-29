3684aa6f48fa3851eba5932ccf3cfb295dfefa3f
Woongcheol Shin <1994_100@naver.com>
2022-01-29 15:11:29 +0900

    add b1001

---
+++ java/src/b1001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class b1001 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a-b);
+        in.close();
+    }
+}
