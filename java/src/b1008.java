7eb4b8868260d67922d43a7903389a94f656f282
Woongcheol Shin <1994_100@naver.com>
2022-01-29 15:17:52 +0900

    add b1008

---
+++ java/src/b1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class b1008 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        double answer = a*1.0/b;
+        System.out.println(answer);
+        in.close();
+    }
+}
