854eb7c4a4261602e3bc7675d6e675fe7265bdbe
Woongcheol Shin <1994_100@naver.com>
2022-01-29 15:19:12 +0900

    update b1008

--- java/src/b1008.java
+++ java/src/b1008.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class b1008 {
+    // https://seeminglyjs.tistory.com/149 참고
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
